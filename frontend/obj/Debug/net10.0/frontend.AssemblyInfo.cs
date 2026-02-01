34981a6da22d626267fff07c049cfd6e0470be81
dawooda <adawood1@my.bcit.ca>
2026-02-01 01:37:16 -0800

    changed applicationUrl to 3000

--- frontend/obj/Debug/net10.0/frontend.AssemblyInfo.cs
+++ frontend/obj/Debug/net10.0/frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb884d6c5fe6d07075406de8fa246a1dfb380ab0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d76ae20ae2df55e8033fa346dbabd26a63f2ce91")]
 [assembly: System.Reflection.AssemblyProductAttribute("frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

08981d462c169b696d7613df1072bee56c704454
Kai Hong Fung <kfung75@my.bcit.ca>
2026-02-12 02:22:47 -0800

    added timesheet and timesheet row entities

--- sql/init_scripts/project_management_db_ddl.sql
+++ sql/init_scripts/project_management_db_ddl.sql
@@ -2,6 +2,8 @@ CREATE DATABASE IF NOT EXISTS Project_Management;
 
 USE Project_Management;
 
+DROP TABLE IF EXISTS Timesheet_Row; 
+DROP TABLE IF EXISTS Timesheet;    
 DROP TABLE IF EXISTS Work_Package_Assignment;
 DROP TABLE IF EXISTS Project_Assignment;
 DROP TABLE IF EXISTS Work_Package;
@@ -12,13 +14,15 @@ DROP TABLE IF EXISTS Employee_E_Signature;
 DROP TABLE IF EXISTS Labor_Grade;
 
 CREATE TABLE Labor_Grade(
-    labor_grade_id INT PRIMARY KEY
+    labor_grade_id INT PRIMARY KEY,
+    grade_code VARCHAR(2) NOT NULL, 
+    charge_rate DECIMAL(10,2) NOT NULL
 );
 
 CREATE TABLE Rate_History(
-    rate_history INT PRIMARY KEY,
+    rate_history_id INT PRIMARY KEY,
     labor_grade_id INT NOT NULL,
-    charge_rate DECIMAL NOT NULL,
+    charge_rate DECIMAL(10,2) NOT NULL,
     start_date DATE NOT NULL,
     end_date DATE,
     FOREIGN KEY (labor_grade_id) REFERENCES Labor_Grade(labor_grade_id)
@@ -26,7 +30,7 @@ CREATE TABLE Rate_History(
 
 CREATE TABLE Employee_E_Signature(
     emp_e_sig_id INT PRIMARY KEY,
-    signature_data VARBINARY(255) NOT NULL,
+    signature_data LONGBLOB NOT NULL,
     signed_at DATETIME NOT NULL
 );
 
@@ -38,8 +42,8 @@ CREATE TABLE Employee(
     system_role ENUM('HR', 'ADMIN', 'EMPLOYEE'),
     emp_e_sig_id INT NOT NULL,
     labor_grade_id INT NOT NULL,
-    supervisor_id INT NOT NULL,
-    vacation_sick_balance DECIMAL,
+    supervisor_id INT,
+    vacation_sick_balance DECIMAL(10,2),
     expected_weekly_hours DECIMAL(3,1) NOT NULL,
     FOREIGN KEY (emp_e_sig_id) REFERENCES Employee_E_Signature(emp_e_sig_id),
     FOREIGN KEY (labor_grade_id) REFERENCES Labor_Grade(labor_grade_id),
@@ -112,4 +116,34 @@ CREATE TABLE Work_Package_Assignment(
     assignment_date DATE NOT NULL,
     FOREIGN KEY (emp_id) REFERENCES Employee(emp_id),
     FOREIGN KEY (wp_id) REFERENCES Work_Package(wp_id)
+);
+
+CREATE TABLE Timesheet(
+    ts_id INT NOT NULL PRIMARY KEY,
+    emp_id INT NOT NULL,
+    week_ending DATE NOT NULL,
+    approver_id INT NOT NULL,
+    approval_status BOOLEAN NOT NULL,
+    return_comment TEXT,
+    emp_e_sig_id INT, 
+    FOREIGN KEY (emp_id) REFERENCES Employee(emp_id),
+    FOREIGN KEY (approver_id) REFERENCES Employee(emp_id),
+    FOREIGN KEY (emp_e_sig_id) REFERENCES Employee_E_Signature(emp_e_sig_id)
+);
+
+CREATE TABLE Timesheet_Row(
+    ts_row_id INT NOT NULL PRIMARY KEY,
+    ts_row_monday DECIMAL(4,1) NOT NULL,
+    ts_row_tuesday DECIMAL(4,1) NOT NULL,
+    ts_row_wednesday DECIMAL(4,1) NOT NULL,
+    ts_row_thursday DECIMAL(4,1) NOT NULL,
+    ts_row_friday DECIMAL(4,1) NOT NULL,
+    ts_row_saturday DECIMAL(4,1) NOT NULL,
+    ts_row_sunday DECIMAL(4,1) NOT NULL,
+    labor_grade_id INT NOT NULL,
+    wp_id VARCHAR(255) NOT NULL,
+    ts_id INT NOT NULL,
+    FOREIGN KEY (labor_grade_id) REFERENCES Labor_Grade(labor_grade_id),
+    FOREIGN KEY (wp_id) REFERENCES Work_Package(wp_id),
+    FOREIGN KEY (ts_id) REFERENCES Timesheet(ts_id)
 );

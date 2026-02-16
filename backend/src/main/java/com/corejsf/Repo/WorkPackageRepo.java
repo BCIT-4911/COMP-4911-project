7cf24acf59c91d5e62756b1603e8aff83f8c49f8
Lucas Liu <lucasqhliu@gmail.com>
2026-02-15 21:30:55 -0800

    implemented Repo class to find all the workpackage with the id

---
+++ backend/src/main/java/com/corejsf/Repo/WorkPackageRepo.java
@@ -0,0 +1,30 @@
+package com.corejsf.Repo;
+
+import java.util.List;
+
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import com.corejsf.Entity.WorkPackage;
+
+@Stateless
+public class WorkPackageRepo {
+
+    @PersistenceContext(unitName = "pmPU")
+    private EntityManager em;
+
+    public WorkPackage findById(String wpId) {
+        return em.find(WorkPackage.class, wpId);
+    }
+
+    public List<WorkPackage> findDirectChildren(String parentWpId) {
+        return em.createQuery(
+                "SELECT w FROM WorkPackage w " +
+                "WHERE w.parentWorkPackage.wpId = :pid " +
+                "ORDER BY w.wpId",
+                WorkPackage.class
+        ).setParameter("pid", parentWpId)
+         .getResultList();
+    }
+}

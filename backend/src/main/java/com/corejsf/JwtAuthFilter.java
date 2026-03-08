3af8cf8d4a1d922f047580ec0b9761bc14df177e
Russell melchiorre <russellstevenmelchiorre@gmail.com>
2026-03-07 18:16:06 -0800

    Created filter for that checks all /api/* requests except to the login for a valid token then adds the employee to the context if valid, currently uses placeholder for validation as that has not been completed at this time

---
+++ backend/src/main/java/com/corejsf/JwtAuthFilter.java
@@ -0,0 +1,73 @@
+package com.corejsf;
+
+import java.io.IOException;
+
+import com.corejsf.Entity.Employee;
+
+import jakarta.annotation.Priority;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.Provider;
+
+/**
+ * A filter for JWT that will authenticate a request if it is going to anything under /api other then the login page.
+ * Returns 401 unauthorized if the token is invalid or there is something wrong with it.
+ * sets the authenticated employee in the request so that other controllers can use it after.
+ * 
+ * NOTE for controllers:
+ * to get the authenticated employee from a request you can do this:
+ * Employee employee = (Employee) requestContext.getProperty(JwtAuthFilter.AUTHENTICATED_EMPLOYEE);
+ * 
+ * @Author Russell
+ * @verson 1.0
+ */
+@Provider
+@Priority(1000) //Authentication priority to make sure it runs before other things
+public class JwtAuthFilter implements ContainerRequestFilter {
+
+    @PersistenceContext(unitName = "project-management-pu")
+    private EntityManager em;
+    
+    public static final String AUTHENTICATED_EMPLOYEE = "authenticatedEmployee";
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        String requestPath = requestContext.getUriInfo().getPath();
+
+        //If the request is for the login page for not for an api/* page then return and let the request carry on
+        if(!requestPath.startsWith("api/") || requestPath.startsWith("api/auth/login")){
+            return;
+        }
+
+        //Get the auth header from the request and then get the token from it.
+        String authHeader = requestContext.getHeaderString("Authorization");
+        String token;
+        if(authHeader != null && authHeader.startsWith("Bearer ")){
+            token = authHeader.substring("Bearer ".length());
+        } else {
+            requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
+            return;
+        }   
+
+        //Check if the token is valid
+        //the check does not exist yet so for a test all employees are set to an ID of 5.
+        //should be something like Interger empId = JwtService.validateToken(token) then check if null
+        Integer empId = 5; 
+
+        //fetch the employee from the tokem.
+        Employee employee = em.find(Employee.class, empId);
+        if(employee == null){
+            requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
+            return;
+        }
+
+        //set the employee in the request
+        requestContext.setProperty(AUTHENTICATED_EMPLOYEE, employee);
+
+    }
+
+
+}

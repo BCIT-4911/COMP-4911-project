1c86091eb6552386503665a87c4d76b9cc643546
Raymond <raymondyang59@gmail.com>
2026-03-10 20:39:12 -0700

    fixed merge issue

--- frontend/Pages/Employees.cshtml.cs
+++ frontend/Pages/Employees.cshtml.cs
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 using Microsoft.AspNetCore.Mvc;
->>>>>>> 4c14e097edde2f44752d0f5b5880dd18620fdc3c
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace frontend.Pages;
@@ -10,21 +7,11 @@ public class EmployeesModel : PageModel
 {
     private readonly IConfiguration _config;
 
-<<<<<<< HEAD
-    public string ApiBaseUrl { get; private set; } = "";
-
-=======
->>>>>>> 4c14e097edde2f44752d0f5b5880dd18620fdc3c
     public EmployeesModel(IConfiguration config)
     {
         _config = config;
     }
 
-<<<<<<< HEAD
-    public void OnGet()
-    {
-        ApiBaseUrl = _config["ApiBaseUrl"] ?? "";
-=======
     public IActionResult OnGet()
     {
         var token = HttpContext.Session.GetString("JWT");
@@ -36,6 +23,5 @@ public class EmployeesModel : PageModel
             return RedirectToPage("/Index");
 
         return Page();
->>>>>>> 4c14e097edde2f44752d0f5b5880dd18620fdc3c
     }
 }

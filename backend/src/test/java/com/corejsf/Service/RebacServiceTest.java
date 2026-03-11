c45a60b6126a5b086be76920292ec62f03bf4964
kaid <kkrawchuk5@my.bcit.ca>
2026-03-10 23:02:18 -0700

    rebac service unit testing

--- backend/src/test/java/com/corejsf/Service/RebacServiceTest.java
+++ backend/src/test/java/com/corejsf/Service/RebacServiceTest.java
@@ -3,6 +3,7 @@ package com.corejsf.Service;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import com.corejsf.Entity.SystemRole;
+import com.corejsf.Entity.Employee;
 
 public class RebacServiceTest {
 
@@ -11,5 +12,108 @@ public class RebacServiceTest {
     @Test
     void operationsManagerCanCreateProject() {
         assertTrue(rebacService.canCreateProject(SystemRole.OPERATIONS_MANAGER));
+    }
+
+    @Test
+    void hrCannotCreateProject() {
+        assertFalse(rebacService.canCreateProject(SystemRole.HR));
+    }
+
+    @Test
+    void employeeCannotCreateProject() {
+        assertFalse(rebacService.canCreateProject(SystemRole.EMPLOYEE));
+    }
+
+    @Test
+    void hrCanManageEmployees() {
+        assertTrue(rebacService.canManageEmployees(SystemRole.HR));
+    }
+
+    @Test
+    void operationsManagerCannotManageEmployees() {
+        assertFalse(rebacService.canManageEmployees(SystemRole.OPERATIONS_MANAGER));
+    }
+
+    @Test
+    void employeeCannotManageEmployees() {
+        assertFalse(rebacService.canManageEmployees(SystemRole.EMPLOYEE));
+    }
+
+    @Test
+    void isOperationsManagerReturnsTrueForOperationsManager() {
+        Employee emp = new Employee();
+        emp.setSystemRole(SystemRole.OPERATIONS_MANAGER);
+        assertTrue(rebacService.isOperationsManager(emp));
+    }
+
+    @Test
+    void isOperationsManagerReturnsFalseForHr() {
+        Employee emp = new Employee();
+        emp.setSystemRole(SystemRole.HR);
+        assertFalse(rebacService.isOperationsManager(emp));
+    }
+
+    @Test
+    void isOperationsManagerReturnsFalseForEmployee() {
+        Employee emp = new Employee();
+        emp.setSystemRole(SystemRole.EMPLOYEE);
+        assertFalse(rebacService.isOperationsManager(emp));
+    }
+
+    @Test
+    void isHrReturnsTrueForHrEmployee() {
+        Employee emp = new Employee();
+        emp.setSystemRole(SystemRole.HR);
+        assertTrue(rebacService.isHr(emp));
+    }
+
+    @Test
+    void isHrReturnFalseForNonHrEmployee() {
+        Employee emp = new Employee();
+        emp.setSystemRole(SystemRole.EMPLOYEE);
+        assertFalse(rebacService.isHr(emp));
+    }
+
+    @Test
+    void isHrReturnsFalseForNullEmployee() {
+        assertFalse(rebacService.isHr(null));
+    }
+
+    @Test
+    void employeeObjectCanCreateProjectWhenSetToOperationsManager() {
+        Employee emp = new Employee();
+        emp.setSystemRole(SystemRole.OPERATIONS_MANAGER);
+        assertTrue(rebacService.canCreateProject(emp));
+    }
+
+    @Test
+    void employeeObjectCannotCreateProjectWhenSetToHr() {
+        Employee emp = new Employee();
+        emp.setSystemRole(SystemRole.HR);
+        assertFalse(rebacService.canCreateProject(emp));
+    }
+
+    @Test
+    void nullEmployeeCannotCreateProject() {
+        assertFalse(rebacService.canCreateProject((Employee) null));
+    }
+
+    @Test
+    void employeeObjectCanManageEmployeeWhenSetToHr() {
+        Employee emp = new Employee();
+        emp.setSystemRole(SystemRole.HR);
+        assertTrue(rebacService.canManageEmployees(emp));
+    }
+
+    @Test 
+    void employeeObjectCannotManageEmployeeWhenSetToOperationsManager() {
+        Employee emp = new Employee();
+        emp.setSystemRole(SystemRole.OPERATIONS_MANAGER);
+        assertFalse(rebacService.canManageEmployees(emp));
+    }
+
+    @Test
+    void nullEmployeeCannotManageEmployee() {
+        assertFalse(rebacService.canManageEmployees((Employee) null));
     }
 }

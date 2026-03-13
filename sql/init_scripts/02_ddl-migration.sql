162d7028454617f10b110a1aedce82dc85ba1a38
Kai Hong Fung <kfung75@my.bcit.ca>
2026-03-13 03:00:11 -0700

    Removed redundancies to follow requirements and tested

---
+++ sql/init_scripts/02_ddl-migration.sql
@@ -0,0 +1,13 @@
+USE Project_Management;
+
+ALTER TABLE Timesheet
+    ADD COLUMN status ENUM('DRAFT', 'SUBMITTED', 'APPROVED', 'RETURNED') NOT NULL DEFAULT 'DRAFT';
+
+UPDATE Timesheet
+SET status = CASE
+    WHEN approved = b'1' THEN 'SUBMITTED'
+    ELSE 'DRAFT'
+END;
+
+ALTER TABLE Timesheet
+    DROP COLUMN approved;

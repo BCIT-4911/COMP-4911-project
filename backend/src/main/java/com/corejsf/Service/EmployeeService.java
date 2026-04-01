c168b854df821ecffd6d9195cf602a5e706e5d84
Cyrus Wong <kwong582@my.bcit.ca>
2026-03-31 23:25:16 -0700

    Both update and deleting resource endpoints are completed with ACID rules followed. Transaction will rollback if update process fails.

--- backend/src/main/java/com/corejsf/Service/EmployeeService.java
+++ backend/src/main/java/com/corejsf/Service/EmployeeService.java
@@ -1,5 +1,8 @@
 package com.corejsf.Service;
 
+import com.corejsf.DTO.EmployeeManagerUpdateDto;
+import jakarta.ws.rs.BadRequestException;
+import jakarta.ws.rs.InternalServerErrorException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -7,18 +10,22 @@ import java.util.List;
 import org.mindrot.jbcrypt.BCrypt;
 
 import com.corejsf.DTO.EmployeeCreateDTO;
+import com.corejsf.DTO.EmployeeSelfUpdateDto;
+
 import com.corejsf.Entity.Employee;
 import com.corejsf.Entity.EmployeeESignature;
 import com.corejsf.Entity.LaborGrade;
 
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.PersistenceContext;
 import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.core.Response;
 
 /**
- * Service class for managing employee data.
- * Provides methods for retrieving and creating employee records.
+ * Service class for managing employee data. Provides methods for retrieving and
+ * creating employee records.
  */
 @Stateless
 public class EmployeeService {
@@ -28,6 +35,7 @@ public class EmployeeService {
 
     /**
      * Retrieves all employees.
+     *
      * @return a list of all employees.
      */
     public List<Employee> getAllEmployees() {
@@ -37,9 +45,11 @@ public class EmployeeService {
 
     /**
      * Retrieves an employee by ID.
+     *
      * @param id the ID of the employee to retrieve.
      * @return the employee with the specified ID.
-     * @throws NotFoundException if the employee with the specified ID is not found.
+     * @throws NotFoundException if the employee with the specified ID is not
+     * found.
      */
     public Employee getEmployee(int id) {
         Employee emp = em.find(Employee.class, id);
@@ -51,6 +61,7 @@ public class EmployeeService {
 
     /**
      * Creates a new employee.
+     *
      * @param dto the data transfer object containing new employee details.
      * @return the created employee.
      */
@@ -58,7 +69,7 @@ public class EmployeeService {
         EmployeeESignature sig = new EmployeeESignature();
         Employee newEmp = new Employee();
 
-        sig.setSignatureData(new byte[] { 0x00 });
+        sig.setSignatureData(new byte[]{0x00});
         sig.setSignedAt(LocalDateTime.now());
         em.persist(sig);
 
@@ -73,5 +84,152 @@ public class EmployeeService {
         newEmp.setESignature(sig);
         em.persist(newEmp);
         return newEmp;
+    }
+
+    /**
+     * This update employee function validates all requested fields before processing them to fulfill ACID rules
+     *
+     * @param id The employee ID
+     * @param dto An EmployeeManagerUpdateDto class which contains the personal details of the employee
+     * @return An updated Employee record to the resource end point
+     */
+    public Employee updateEmployee(final int id, final EmployeeManagerUpdateDto dto)
+    {
+        Employee emp = em.find(Employee.class, id);
+
+        if (emp == null)
+        {
+            throw new NotFoundException("The specific employee with ID " + id + " cannot be found.");
+        }
+
+        if (dto.firstName() != null && dto.firstName().isBlank())
+        {
+            throw new BadRequestException("The new first name cannot be blank (filled only with space character).");
+        }
+
+        if (dto.lastName() != null && dto.lastName().isBlank())
+        {
+            throw new BadRequestException("The new last name cannot be blank (filled only with space character).");
+        }
+
+        LaborGrade newLaborGrade = em.find(LaborGrade.class, dto.laborGradeId());
+
+        if (dto.laborGradeId() > 0 && newLaborGrade  == null)
+        {
+            throw new BadRequestException("The new labour grade ID " + dto.laborGradeId() + " is invalid.");
+        }
+
+        Employee newSupervisor = em.find(Employee.class, dto.supervisorId());
+
+        if (dto.supervisorId() > 0 && newSupervisor == null)
+        {
+            throw new BadRequestException("The new supervisor ID " + dto.supervisorId() + " is invalid.");
+        }
+
+        EntityTransaction transaction = em.getTransaction();
+
+        try
+        {
+            transaction.begin();
+
+            if(dto.firstName() != null)
+            {
+                emp.setEmpFirstName(dto.firstName());
+            }
+
+            if(dto.lastName() != null)
+            {
+                emp.setEmpLastName(dto.lastName());
+            }
+
+            if(dto.supervisorId() > 0)
+            {
+                emp.setSupervisor(newSupervisor);
+            }
+
+            if(dto.laborGradeId() > 0)
+            {
+                emp.setLaborGrade(newLaborGrade);
+            }
+
+            if(dto.systemRole() != null)
+            {
+                emp.setSystemRole(dto.systemRole());
+            }
+
+            transaction.commit();
+        }
+        catch(Exception ex)
+        {
+            transaction.rollback();
+            throw new InternalServerErrorException("Unknown error has occurred during the update: " +
+                                                   ex.getClass().getName() + " - " +
+                                                   ex.getMessage());
+        }
+
+        return emp;
+    }
+
+    /**
+     * Allow an employee to self-update their own information
+     *
+     * Because the entity manager is only injected into the employee service, but not at the resource end point,
+     * the resource end-point would not be able to check if the user id in the auth context actually exists or not.
+     * THerefore, the RESTFul response is generated from this method directly upon calling.
+     */
+    public Response employeeSelfUpdatePassword(final int id, final EmployeeSelfUpdateDto dto) {
+        Employee emp = em.find(Employee.class, id);
+
+        if (emp == null) {
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        }
+
+        if (dto.empPassword() == null || dto.empPassword().isBlank()) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Bad Request: The new password cannot be " +
+                                                                       "omitted or be blank").build();
+        }
+
+        emp.setEmpPassword(BCrypt.hashpw(dto.empPassword().trim(), BCrypt.gensalt()));
+
+        // No-Content means status 204 code with a successful update result
+        return Response.noContent().build();
+    }
+
+    /**
+     * An employee service which can be invoked by a manager to delete an employee.
+     *
+     * @param id The ID of the employee which is to be deleted.
+     * @return The HTTP Response Code with a detailed message to tell if the deletion was successful or not
+     */
+    public Response deleteEmployee(final int id)
+    {
+        Employee emp = em.find(Employee.class, id);
+
+        if (emp == null)
+        {
+            return Response.status(Response.Status.NOT_FOUND).entity("The employee with ID " + id + " cannot be found" +
+                                                                     ".").build();
+        }
+
+        EntityTransaction transaction = em.getTransaction();
+
+        try
+        {
+            transaction.begin();
+            em.remove(emp);
+            transaction.commit();
+        }
+        catch(final Exception ex)
+        {
+            transaction.rollback();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Unknown error has occurred when " +
+                                                                                 "deleting the employee with ID " + id +
+                                                                                 ": " + ex.getClass().getName() +
+                                                                                 " - " + ex.getMessage()).build();
+        }
+
+        return Response.status(Response.Status.OK).entity("The employee with ID " + id  + " was successfully deleted.")
+                       .build();
+
     }
 }
--- backend/src/main/java/com/corejsf/api/EmployeeResource.java
+++ backend/src/main/java/com/corejsf/api/EmployeeResource.java
@@ -1,5 +1,11 @@
 package com.corejsf.api;
 
+import com.corejsf.DTO.EmployeeManagerUpdateDto;
+import com.corejsf.DTO.EmployeeSelfUpdateDto;
+import jakarta.ws.rs.BadRequestException;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.InternalServerErrorException;
+import jakarta.ws.rs.NotFoundException;
 import java.util.List;
 
 import com.corejsf.DTO.EmployeeCreateDTO;
@@ -11,6 +17,7 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
@@ -90,5 +97,95 @@ public class EmployeeResource {
         }
         Employee created = employeeService.createEmployee(dto);
         return Response.status(Response.Status.CREATED).entity(created).build();
+    }
+
+    /**
+     * Updates an existing employee.
+     * Only accessible to users with employee management permissions, or the employee himself / herself.
+     *
+     * @param dto The data transfer object containing new employee details.
+     *            Fields with null data / smaller than or equal to 0 will be ignored for update.
+     * @return The updated employee object (if updated successfully) and corresponding HttpStatusCode
+     */
+    @PUT
+    @Path("/{id}")
+    public Response update(@PathParam("id") final int id, final EmployeeManagerUpdateDto dto)
+    {
+        if (!rebacService.canManageEmployees(authContext.getSystemRole()))
+        {
+            return forbidden();
+        }
+
+        Employee updated = null;
+
+        try
+        {
+            updated = employeeService.updateEmployee(id, dto);
+        }
+        catch (NotFoundException ex)
+        {
+            return Response.status(Response.Status.NOT_FOUND).entity(ex.getMessage()).build();
+        }
+        catch (BadRequestException ex)
+        {
+            return Response.status(Response.Status.BAD_REQUEST).entity(ex.getMessage()).build();
+        }
+        catch (InternalServerErrorException ex)
+        {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
+        }
+        catch (Exception ex)
+        {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Unknown error has occured when " +
+                                                                                 "updating the employee: " +
+                                                                                 ex.getClass().getName() + " - " +
+                                                                                 ex.getMessage()).build();
+        }
+
+        if (updated == null)
+        {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Unknown error has occured when " +
+                                                                                 "updating the employee: Please " +
+                                                                                 "contact system administrator for a " +
+                                                                                 "fix").build();
+        }
+        else
+        {
+            return Response.ok().entity(updated).build();
+        }
+    }
+
+    /**
+     * An employee updates his / her own password.
+     * Mote: This resource end-point will heavily depend on whether the injected authContext was legitimate
+     *
+     * @param dto An EmployeeSelfUpdateDto object that contains the new password.
+     * @return An HTTP Status Code with content encoded in the acceptable format arranged by Jakarta EE
+     *         Initial status codes (Mar 31, 2026) include "Success", "Unauthorized", and "No-Content (204)"
+     */
+    @POST
+    @Path("/employee-self-update-password")
+    public Response selfUpdatePassword(final EmployeeSelfUpdateDto dto)
+    {
+        final int empId = authContext.getEmpId();
+
+        return employeeService.employeeSelfUpdatePassword(empId, dto);
+    }
+
+    /**
+     * A RESTFul resource endpoint which allows managers to delete an employee after their role is verified.
+     * @param id The ID of the employee to be deleted.
+     * @return The HTTP status code with a specific message to tell the deletion was success or not.
+     */
+    @DELETE
+    @Path("/{id}")
+    public Response deleteEmployee(@PathParam("id") final int id)
+    {
+        if(rebacService.canManageEmployees(authContext.getSystemRole()))
+        {
+            return forbidden();
+        }
+
+        return employeeService.deleteEmployee(id);
     }
 }
